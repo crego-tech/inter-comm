a3b4cabe5c874384f53e0b9e3f319b022b05ead6
Abhishek Sharma <abhishm20@gmail.com>
2023-10-04 01:27:48 +0530

    Initial Commit

--- inter_comm/apis/__init__.py
+++ inter_comm/apis/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from .accounting import *
+from .crm import *
+from .disburse import *
+from .gl import *
+from .identity import *
+from .unr import *
